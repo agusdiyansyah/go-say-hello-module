0bc2d7938c21ccfb18f541398f063467b86f1ef4
agusDiyansyah <agusdiyansyah.dev@gmail.com>
2022-11-16 18:43:57 +0700

    update module

--- say_hello_test.go
+++ say_hello_test.go
@@ -3,15 +3,19 @@ package gosayhellomodule
 import "testing"
 
 func TestSayHello(t *testing.T) {
+	type args struct {
+		name string
+	}
 	tests := []struct {
 		name string
+		args args
 		want string
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SayHello(); got != tt.want {
+			if got := SayHello(tt.args.name); got != tt.want {
 				t.Errorf("SayHello() = %v, want %v", got, tt.want)
 			}
 		})
